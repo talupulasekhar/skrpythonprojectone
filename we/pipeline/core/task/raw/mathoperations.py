1d7aa5b9396a89ced122bb559692752ce50c421b
sekhar talupula <sekher.talupula@gmail.com>
2024-03-30 16:48:44 +0530

    updated code

---
+++ we/pipeline/core/task/raw/mathoperations.py
@@ -0,0 +1,23 @@
+import os
+
+
+def max(x:int , y:int) -> int :
+    if x > y:
+        return x
+    else:
+        return y
+
+
+
+def min(x:int , y:int) -> int :
+    if x > y:
+        return y
+    else:
+        return x
+
+
+
+
+
+
+
