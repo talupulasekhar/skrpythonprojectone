47ec45377bc7e5346511eda329359bc993a19e89
sekhar talupula <sekher.talupula@gmail.com>
2024-03-30 15:44:02 +0530

    Removing ignored files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='skrpythonprojectone',
+	version='0.1',
+	description='A sample Python package',
+	author='sekher talupula',
+	author_email='sekher.talupula@gmail.com',
+	install_requires=[
+		'numpy',
+		'pandas',
+	],
+)
+
---
+++ we/pipeline/core/task/raw/calculator.py
@@ -0,0 +1,15 @@
+import os
+
+def add(x: int, y:int):
+    return x+y
+
+def sub(x: int, y:int):
+    return x-y
+
+def mul(x: int, y:int):
+    return x*y
+
+def div(x: int, y:int):
+    return x/y
+
+
